44b2f8fe65d685106f85a0f2ce1975a2387c351a
thales <thales.ume@gmail.com>
2023-02-14 23:29:02 -0300

    start components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthenticateComponent } from './components/authenticate/authenticate.component';
+import { UsersComponent } from './components/users/users.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainComponent,
+    LogoutComponent,
+    LoginComponent,
+    AuthenticateComponent,
+    UsersComponent,
+    MoviesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
