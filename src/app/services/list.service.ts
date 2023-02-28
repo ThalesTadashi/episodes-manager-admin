47ba2e0443d350df0b08d19a5d14cb732f08ccef
thales <thales.ume@gmail.com>
2023-02-27 22:21:06 -0300

    fix

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Movie } from '../movie';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
+  private apiUrl = 'http://localhost:3000/movies'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  remove(movies: Movie[], movie: Movie){
+    return movies.filter((a) => movie.name !== a.name);
+  }
+
+  getAll(): Observable<Movie[]>{
+      return this.http.get<Movie[]>(this.apiUrl);
+  }
 }
