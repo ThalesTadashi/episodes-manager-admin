8be007cc196c43e08708d3117bfbccae2d5b4c80
thales <thales.ume@gmail.com>
2023-03-24 20:12:02 -0300

    auth login

--- src/app/components/authenticate/authenticate.component.ts
+++ src/app/components/authenticate/authenticate.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
+interface AuthenticationResponse {
+  success: boolean;
+  message: string;
+  token?: string;
+}
+@Injectable({
+  providedIn:'root'
+})
 @Component({
   selector: 'app-authenticate',
   templateUrl: './authenticate.component.html',
   styleUrls: ['./authenticate.component.scss']
 })
 export class AuthenticateComponent {
+  private apiUrl = 'http://localhost:3000/auth';
 
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<AuthenticationResponse> {
+    return this.http.post<AuthenticationResponse>(`${this.apiUrl}/login`, { email, password })
+      .pipe(
+        map(response => {
+          if (response.success && response.token) {
+            localStorage.setItem('token', response.token);
+          }
+          return response;
+        })
+      );
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthenticateComponent } from '../authenticate/authenticate.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+
+  email: string;
+  password: string;
+  errorMessage: string | undefined;
+
+  constructor(
+    private authService: AuthenticateComponent, 
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  showMessage(t: string, m: string, type: any = 'warning') {
+    Swal.fire({
+      title: t,
+      html: m,
+      icon: type,
+      confirmButtonColor: '#032E58',
+  });
+}
+
+login() {
+  this.authService.login(this.email, this.password).subscribe(
+    response => {
+      if (response.success) {
+        this.router.navigate(['/']);
+      } else {
+        this.errorMessage = response.message;
+      }
+    },
+    error => {
+      console.error(error);
+      this.showMessage('Atenção', 'E-mail ou senha incorretos', 'warning')
+    }
+  );
+}
 
 }
