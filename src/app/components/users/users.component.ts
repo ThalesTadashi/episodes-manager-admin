1969207a323fcbd1c9421ecb58f17c66ad8b2e38
thales <thales.ume@gmail.com>
2023-04-13 22:34:07 -0300

    Starting the project, finally

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -4,7 +4,9 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { CreateUserInput } from 'src/generated/graphql';
 import { CreateUserGQL } from 'src/generated/graphql';
+import Swal from "sweetalert2";
 
+declare var $: any;
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -12,17 +14,18 @@ import { CreateUserGQL } from 'src/generated/graphql';
 })
 export class UsersComponent {
   form = new FormGroup({
-    name: new FormControl('teste', [Validators.required]), // Validators.pattern(/\s/)
+    name: new FormControl('Name', [Validators.required]),
+    nickName: new FormControl('Nick', [Validators.required]),
     email: new FormControl('test@test.com', [Validators.required, Validators.pattern('^[^ ]+@[^ ]+\\.[a-z]{2,7}$')]),
-    phone: new FormControl(''),
+    phone: new FormControl('',[Validators.required]),
+    profile: new FormControl('user'),
     sex: new FormControl('', [Validators.required]),
   });
 
   constructor(
     public router: Router,
     public createUserGQL: CreateUserGQL
-    // public createUser: CreateUserGQL
-    ) {
+  ) {
   }
   getErrorMessage(s: string) {
     return s === 'required' ? 'Campo obrigatório' : 'Campo inválido';
@@ -30,28 +33,42 @@ export class UsersComponent {
   ngOnInit(): void {
   }
 
+  showMessage(t: string, m: string, type: any = 'warning') {
+    Swal.fire({
+        title: t,
+        html: m,
+        icon: type,
+        confirmButtonColor: '#032E58',
+    });
+}
+
+showModal(idModal: string) {
+  $('#' + idModal).modal('show');
+}
   onSubmit() {
     if (!this.form.valid) {
       console.log('Preencha os campos obrigatórios!');
       return;
     }
-    console.log('user cadastrado!');
-  }
 
-  createUser(){
+    console.log('user cadastrado!');
+    console.log(this.form.value);
+
+    this.showModal('userModal')
 
     const data: CreateUserInput = {
-      email: 'teste@teste.com',
-      phone: '123123123',
-      name: 'XXX',
-      profile: 'admin',
+      email: this.form.value.email || '',
+      phone: this.form.value.phone || '',
+      name: this.form.value.name || '',
+      profile: this.form.value.profile || '',
     }
-
-    
-    this.createUserGQL.mutate({data: data}).subscribe((result) => {
-    
-  });
+    this.createUserGQL.mutate({ data: data }).subscribe((result) => {
+      this.showMessage('Sucesso', 'Usuário criado', 'success');
+            
+    });
   }
+
+
 
 
 }
